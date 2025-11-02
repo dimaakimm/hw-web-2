788ddcb33c3dcf3d1c603bff812f2fed9c1b4371
dimaakimm <dmitriykim2005@gmail.com>
2025-11-02 16:44:14 +0300

    fix

--- dist/assets/index-3K5GrJzy.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const a="/",o=a.replace(/\/+$/,"/")+"jobs.html";location.pathname+location.search+location.hash!==o&&location.replace(o);
---
+++ dist/assets/index-l8ZlRO-n.js
@@ -0,0 +1,1 @@
+import"./global-DDRV_GHX.js";const a="/hw-web-2/",o=a.replace(/\/+$/,"/")+"jobs.html";location.pathname+location.search+location.hash!==o&&location.replace(o);
